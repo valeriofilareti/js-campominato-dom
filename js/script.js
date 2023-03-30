db98dfea8908ada1565577ff28c6f998b47c53b8
valeriofilareti <valerio.filareti@gmail.com>
2023-03-30 15:44:25 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+
+
+const container = document.querySelector('.container');
+const genBtn = document.getElementById('gen-btn');
+const resBtn = document.getElementById('res-btn');
+
+
+
+
+genBtn.addEventListener('click', function () {
+  
+  const difficult = document.querySelector('.diff')
+
+  let totalBoxes;
+  
+  if (difficult.value === 'hard'){
+    totalBoxes = 100;
+  } else if (difficult.value === 'medium') {
+    totalBoxes = 81
+  } else if (difficult.value === 'easy') {
+    totalBoxes = 49;
+  }
+  for (i = 0; i < totalBoxes; i++ ) {
+    let box;
+    if (totalBoxes === 100) {
+      box = generateBoxes('small-box');
+    } else if (totalBoxes === 81) {
+      box = generateBoxes('medium-box')
+    } else if (totalBoxes === 49) {
+      box = generateBoxes('large-box')
+    }
+
+      container.append(box);
+      box.position = i + 1;
+
+      box.addEventListener('click', function () {
+
+        console.log(box.position);
+        this.classList.toggle('active')
+      })
+    }
+})
+
+
+
+
+
+
+
+function generateBoxes (boxN) {
+  const boxElement = document.createElement('div')
+  boxElement.classList.add(boxN)
+  return boxElement
+}
